f0b1c1429e36c88721fd31cd5c9ae9f98bd638f9
michalmetel <michal.metel2@gmail.com>
2026-06-12 10:14:35 +0200

    fix import name 2

--- measurements/views.py
+++ measurements/views.py
@@ -24,7 +24,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
-from django.conf import settingsale tak 
+from django.conf import settings
 
 
 @login_required
